d0c4f4870079ccd43553fd5f7cc067ea8e53edac
ntchris <ntchris@hotmail.com>
2016-12-01 22:29:15 -0500

    Add files via upload
    it can show positive integer

--- tm1637_arduino/tm1637_arduino.ino
+++ tm1637_arduino/tm1637_arduino.ino
@@ -1,4 +1,6 @@
-#include "TM1637_Arduino_Chris.hpp"
+//#include "TM1637_Arduino_Chris.hpp"
+
+
 // Pins connect to arduino
 // if use arduino RX, TX (0,1), no pull up resistor is needed, other pins, need pull up resistor for each.
 #define pin_CLK 2
@@ -367,7 +369,7 @@ void setup()
 
 void loop()
 {
-  TM1637_Arduino_Chris myTM1637(pin_CLK, pin_DIO);
+  //TM1637_Arduino_Chris myTM1637(pin_CLK, pin_DIO);
 
 
   static unsigned long i = 0;
