3b40166be63d07d6a9cb43e3ef80c91f46948fe7
unknown <ntchris@hotmail.com>
2020-07-10 14:00:40 -0400

    V4 and V5
    V5 isfor order
    change some pins

---
+++ DIY_solderStation/arduino_wellerSolderStation_hw_2020_July_v4/DiyWellerSolderStation.hpp
@@ -0,0 +1,161 @@
+#ifndef DiyWellerSolderStation_h
+#define DiyWellerSolderStation_h
+
+#include "Arduino.h"
+#include "FastPID.h"
+
+// DiyWellerSolderStation
+
+// 12V 4.963--4.965V
+// no 12V , only usb: 4.605V
+// 12V and USB 4.967V
+// using only 12V 5V value for calculation
+const float VCC5 = 4.963; // if you change this, must change the excel file as well, column G calculate
+const float ADC1023 = 1023.0;
+//const float OpAmpR1Black = 0.991;  //1 KOhm
+const float OpAmpR2Purple = 0.991; //  1 KOhm
+const float OpAmpRopamp2A = 510.0;  //TC Black  // 510K , R 514
+//const float OpAmpRopamp2B = 510.0;  //TC Black  // 510K , R 514
+//const float OpampGainPurple = OpAmpRopamp2A / OpAmpR2Purple + 1.0; // 515.63
+//const float OpampGain = OpampGainPurple;
+const float OpampGain = OpAmpRopamp2A / OpAmpR2Purple + 1.0; // 515.63
+
+const byte ReadTcDelayMilliSec = 5;
+
+const float Kp = 7.58, Ki = 0.050, Kd = 0.86, FreqHz = 20;
+const bool Sign = false;
+const int Bits = 8;
+
+/*
+  // copied from weller_driver_v0901.c  http://kair.us/projects/weller/index.html
+  // AdcInt List for TC temp:
+  // opAmpOutput Voltage = tcMv * opAmpGain /1000
+  // AdcInt = (opAmpOutput Voltage )/Vcc X 1023
+  static const float ThermalCoupleMvPer10C[] =
+  { 0, 0.130, 0.263, 0.398, 0.537, 0.677, 0.820, 0.966, 1.114, 1.263, 1.415,
+  1.569, 1.725, 1.882, 2.042, 2.203, 2.365, 2.529, 2.694, 2.860, 3.028,
+  3.196, 3.366, 3.536, 3.707, 3.879, 4.052, 4.225, 4.398, 4.572, 4.746,
+  4.920, 5.094, 5.268, 5.442, 5.616, 5.790, 5.963, 6.135, 6.307, 6.479,
+  6.649, 6.819, 6.988, 7.155, 7.322, 7.487, 7.652, 7.814, 7.975, 8.135
+  };
+  const int TcTableSize =  sizeof(ThermalCoupleMvPer10C)/sizeof(ThermalCoupleMvPer10C[0]);
+*/
+
+const uint8_t TempCPerIndex = 10;
+// AdcInt List for TC temp:
+// opAmpOutput Voltage = tcMv * opAmpGain /1000
+// AdcInt = (opAmpOutput Voltage )/Vcc X 1023
+/*
+static int TCAdcIntLookupTable[] =
+{
+  0, 14, 28, 43, 57,   72, 88, 103, 119, 135,   151, 168, 184, 201, 218,  235, 253, 270, 288, 305,
+  323, 341, 359, 378, 396,   414, 433, 451, 470, 488,   507, 525, 544, 563, 581,  600, 618, 637, 655, 673,
+  692, 710, 728, 746, 764,   782, 800, 817, 834, 852,    869
+};
+*/
+//using 4.963 as Vref
+static int TCAdcIntLookupTable[] =
+{ 0,14,28,42,57,   72,87,103,118,134,   150,167,183,200,217,   234,251,269,286,304,   322,340,358,376,394,
+  412,431,449,467,486,   504,523,541,560,578,  597,615,634,652,670,   689,707,725,743,761,
+  778,796,813,831,848,  865
+};
+
+const int TcTableSize = sizeof(TCAdcIntLookupTable) / sizeof(TCAdcIntLookupTable[0]);
+// 51;
+
+
+
+//=======================================================
+class WellerSolderControllerStatus
+{
+
+  public:
+
+    static const byte OFF = 0, ON = 1, UnknownState=3;
+
+  //  bool isHeaterRedEnabled;
+  //  bool isHeaterBrownEnabled;
+    int purpleInt;
+    int redTemp;
+
+    int blackInt;
+    int brownTemp;
+
+    bool isPurpleConnected;
+    bool isBlackConnected;
+    bool isConnected;
+    bool isOnRest;
+    int workStatus;
+    bool fatalError;
+    byte redPwm;
+    byte brownPwm;
+    int evnTemp = 18;
+
+};
+
+
+class DiyWellerSolderStation
+{
+    static const bool debug = false;
+
+    int heaterRedEnablePin = 0;
+    int heaterBrownEnablePin = 0;
+    // purple wire( heater red temp thermal couple wire) to oamp and to arduino
+    int purpleOpAmpReadPin = 0;
+    // black wire (heater brown temp thermal couple wire) to oamp and to arduino
+    int blackOpAmpReadPin = 0;
+    int magnetEnablePin = 0;
+    int magnetDetectPin = 0;
+    // don't check red and brown everytime,
+    // check one each time, check the other next time.
+    bool opampReadIntToIsConnected(int opampValInt);
+
+    void processRedHeaterTemp();
+    void processBrownHeaterTemp();
+    
+  public:
+    float adcIntToVoltage(int adcInt);
+
+    DiyWellerSolderStation(int redEnPin, int brownEnPin,
+                           int purpleOpAmOutPin, int blackOpAmOutPin,
+                           int magnetEn, int magnetDetect);
+    // should be private
+    void initTcAdcIntLookupTable();
+
+    WellerSolderControllerStatus myWellerState;
+    bool checkIsFatalError();
+    void disableHeaterRed();
+    void disableHeaterBrown();
+    void disableAllHeaters();
+    void disableHeater(int heaterEnablePin);
+    void enableHeater(int heaterEnablePin);
+    void enableHeaterRed();
+    void enableHeaterBrown();
+
+    bool checkIsHandleDisconnected();
+    int getPurpleOpAmpOutputInt();
+    int getBlackOpAmpOutputInt();
+    //void updateHeaterRedTemp();
+    //void updateHeaterBrownTemp();
+    //void updateHandleState();
+    bool isPenPutOnRest();
+
+    int getOpampOutputIntToTemperature(int analogReadingInt);
+
+    static int getAnalogAvgReadingInt(int analogReadPin, int avgCount);
+    int convertAdcIntToTemperature(int adcInt);
+    bool selfTestConvertAdcIntToTemperature();
+    int getEnvTemperature();
+    void setHeaterRedPmw(int pwm);
+    void setHeaterBrownPmw(int pwm);
+    int getHeaterRedTemp();
+    int getHeaterBrownTemp();
+    void checkHandleTemp();
+    // for main loop.
+    // do not process red and brown each turn, do it every other time.
+    void processWellerHandleTemp(int targettemp);
+
+};
+
+
+#endif
---
+++ DIY_solderStation/arduino_wellerSolderStation_hw_2020_July_v4/arduino_wellerSolderStation_hw_2020_July.ino
@@ -0,0 +1,774 @@
+// DIY Weller solder pen controller.
+// magnet detection is included.
+//
+// this is for the new hardware version PCB  2020 July 3
+// kty cold temp compensation is included.
+// DIY_weller_solderStationV4.ms14
+
+#include <LiquidCrystal_I2C.h>
+// set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+// with 12V power supply: total power 50ma, unplug lcd, 23ma
+//
+
+#include "superRotaryEncoder.hpp"
+#include "DiyWellerSolderStation.hpp"
+
+
+const bool debug = false;
+
+
+// ******************    Pin definition   ******************
+
+const int BuzzerEnablePin = 2;
+const int I2C_SDA = A4;  // A4, for LCD display SDA
+const int I2C_SCL = A5;  // A5, for LCD display SCL
+
+
+const int HeaterEnableBrown = 3;  // Heater2, Brown,
+const int HeaterEnableRed = 9;    // Heater1, Red  ,
+const int OpampOutput1ForPurplePin = A7; //detect voltage for purple temp after opamp. For Red heater, with magnet reel
+const int OpampOutput2ForBlackPin = A6; // detect voltage for black temp after opamp. For brown heater, with KTY
+// magnet detection: pen on handle, magnet make pin 1 gnd and pin 6 purple connected/shorted,  not on handle(using), r=2K.
+const int MagnetCheckEnablePin = 5; // D6 Magnet Pullup  , provide a 2.5V to check magnet switch for a short time
+const int MagnetDetectPin = A1; // check voltage to see if magnet switch is connected , if so, it's put on handle rest. should shutdown
+
+const int RotaryButtonPin = 10; // using pin change interrupt
+const int RotaryPinA = 11;  // using pin change interrupt
+const int RotaryPinB = 12;  // using pin change interrupt
+
+
+const int KtyEnablePin = 4;  // D4, KtyPullup
+const int TcInputBlackKty = A2;  //    Kty input, black pin
+const int Power12VDetect = A3; // detect if 12V power is connected.
+// ******************    End of Pin definition   ******************
+
+//===========================================
+const int EnvTempC = 18;
+
+const float NotConnectedVoltage = 4.9;
+
+//  const float opampGainBlack = OpAmpRopamp2B * 1.0 / OpAmpR1Black + 1.0;
+// For simplicity, assume opamp gain for input purple and input black are the same,
+
+
+const int MIN_TARGET_TEMP = 54;
+//#define  MIN_TARGET_TEMP  54
+const int MAX_TARGET_TEMP = 360;
+//#define  MAX_TARGET_TEMP  380
+
+const unsigned int MaxPowerOnSec =180;
+unsigned int g_heaterStartTimerSec = 0;
+
+// ****************  For LCD display **********************
+// I2C_SDA = A4;  // A4, for LCD display SDA
+// I2C_SCL = A5;  // A5, for LCD display SCL
+
+
+SuperRotaryEncoder rotEncoder(RotaryPinA, RotaryPinB, RotaryButtonPin );
+
+
+DiyWellerSolderStation station(HeaterEnableRed, HeaterEnableBrown,
+                               OpampOutput1ForPurplePin, OpampOutput2ForBlackPin,
+                               MagnetCheckEnablePin, MagnetDetectPin);
+
+
+void pinInit()
+{
+
+  pinMode(Power12VDetect, INPUT);
+
+  pinMode(HeaterEnableRed, OUTPUT);
+  digitalWrite(HeaterEnableRed, LOW);
+
+  pinMode(HeaterEnableBrown, OUTPUT);
+  digitalWrite(HeaterEnableBrown, LOW);
+  // input pullup useless for A6 A7
+  pinMode(OpampOutput1ForPurplePin, INPUT_PULLUP);
+  // input pullup useless for A6 A7
+  pinMode(OpampOutput2ForBlackPin, INPUT_PULLUP);
+
+  pinMode(MagnetCheckEnablePin, INPUT);
+  pinMode(MagnetDetectPin, INPUT);
+
+  // rotary pins init is in rotary class
+}
+
+
+void displayText(String line1, String line2 = "")
+{
+  //unsigned long startTimeStamp = millis();
+  Serial.println();
+  lcd.setCursor(0, 0);
+  lcd.print(line1);
+  if (line2.length() > 0)
+  {
+    lcd.setCursor(0, 1);
+    lcd.print(line2);
+  }
+  //lcd.noBacklight();
+  //unsigned long endts = millis();
+  // unsigned long delta = endts - startTimeStamp;
+  // time spend is 45ms
+  //Serial.println("start ts:"+ String(startTimeStamp) + " end ts:" + String(endts) + " delta:"+String(delta) );
+}
+
+void showTitle()
+{
+  lcd.clear();
+  displayText("  Super Weller", " Solder Station");
+  //displayTextOneLine("  Super Weller   Solder Station");
+}
+
+void displayInit()
+{
+  //const int DefaultBrightness = 3;
+  //myTM1637.setBrightness(DefaultBrightness);
+  //myTM1637.display("S.t.b.");
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+
+}
+
+void findI2CAddress()
+{
+  Wire.begin();
+  byte count = 0;
+  for (byte i = 8; i < 120; i++)
+  {
+    Wire.beginTransmission (i);
+    if (Wire.endTransmission () == 0)
+    {
+      Serial.print ("Found address: ");
+      Serial.print (i, DEC);
+      Serial.print (" (0x");
+      Serial.print (i, HEX);
+      Serial.println (")");
+      count++;
+      delay (1);  // maybe unneeded?
+    } // end of good response
+  } // end of for loop
+  Serial.println ("Done.");
+  Serial.print ("Found ");
+  Serial.print (count, DEC);
+  Serial.println(" device(s).");
+
+}
+
+
+void encoderInit()
+{
+  rotEncoder.setEncoderValue(MIN_TARGET_TEMP);
+  rotEncoder.setEncoderStep(2);
+  rotEncoder.setMinMaxValue( MIN_TARGET_TEMP, MAX_TARGET_TEMP );
+  rotEncoder.debug = false;
+}
+
+
+void solderStationInit()
+{
+  pinInit();
+  encoderInit();
+  // station.initTcAdcIntLookupTable();
+  //station.selfTestConvertAdcIntToTemperature();
+}
+
+void beepStart()
+{
+  digitalWrite(BuzzerEnablePin, HIGH);
+}
+
+void beepEnd()
+{
+  digitalWrite(BuzzerEnablePin, LOW);
+}
+
+
+void beepForTimeMs(unsigned ms)
+{
+  digitalWrite(BuzzerEnablePin, HIGH);
+  delay(ms);
+  digitalWrite(BuzzerEnablePin, LOW);
+}
+
+
+
+void shortBeep()
+{
+  beepForTimeMs(50);
+}
+
+
+void longBeep()
+{
+  beepForTimeMs(800);
+}
+
+
+bool checkIs12VPowerConnected()
+{
+  static bool lastvalue = false;
+  static unsigned long lastTs = 0;
+  unsigned long ts = millis();
+  const unsigned long Delta = 600;
+  if ((ts - lastTs) > Delta)
+  {
+    bool newval = digitalRead(Power12VDetect);
+    if(lastvalue != newval )
+    {
+      lastvalue = newval;
+      shortBeep();
+    }
+    
+    lastTs = ts;
+  }
+  return lastvalue;
+}
+
+
+
+int getTargetTemperature()
+{
+  int rotaryValue = rotEncoder.getValue();
+  if (debug )
+  {
+    Serial.println("targetTemperature: " + String(rotaryValue) );
+  }
+  return rotaryValue;
+}
+
+void displayTargetTemp(int targettemp)
+{
+  const int temp_x = 13, temp_y = 0;
+  static int oldTarget;
+  if (oldTarget == targettemp)
+  {
+    return;
+  }
+
+  String printtext(targettemp);
+  if (targettemp < 100)
+  {
+    // pad the 2 digits number to 3 digits
+    printtext = " " + printtext;
+  }
+  //Serial.println("displayTargetTemp " + printtext);
+
+  lcd.setCursor(temp_x, temp_y);
+  lcd.print(printtext);
+
+}
+
+
+void displayHandleConnectState()
+{
+  
+  static bool oldIsConnected = true;
+
+  if ( oldIsConnected == station.myWellerState.isConnected)
+  {
+     return;
+  }
+  Serial.println("displayHandleConnectState");
+  oldIsConnected = station.myWellerState.isConnected;
+  String statusText;
+  if( station.myWellerState.isConnected)
+  {
+     statusText = "             ";
+  }else
+  {
+     statusText = "  handle disc?   ";
+     const int x = 0, y = 1;
+  lcd.setCursor(x, y);
+  lcd.print(statusText);
+  }
+  
+}
+
+
+void turnSolderStationOff()
+{
+    station.disableAllHeaters();
+    g_heaterStartTimerSec = 0;
+    station.myWellerState.workStatus = WellerSolderControllerStatus::OFF;
+    if ( debug )
+    {
+      Serial.println("Station is OFF");
+    }
+}
+
+void displayOnOffState()
+{
+  String statusText = "";
+  Serial.println(statusText);
+  if ( station.myWellerState.workStatus == WellerSolderControllerStatus::OFF)
+  {
+    statusText = "OFF  ";
+    Serial.println(statusText);
+  } else {
+
+    statusText = "ON   ";
+  }
+  Serial.println(statusText );
+  static String oldstatusText = "";
+  if ( statusText == oldstatusText)
+  {
+    return;
+  }
+  lcd.setCursor(0, 0);
+  lcd.print(statusText);
+  oldstatusText = statusText;
+}
+
+
+void display12VPower(bool power)
+{
+  String statusText = "";
+  Serial.println(statusText);
+  if (power)
+  {
+    statusText = "12V OK";
+  } else
+  {
+    statusText = "No 12V";
+  }
+  //Serial.println(statusText );
+  static String oldstatusText = "";
+  if ( statusText == oldstatusText)
+  {
+    return;
+  }
+  const int x = 6, y = 0;
+  lcd.setCursor(x, y);
+  lcd.print(statusText);
+  oldstatusText = statusText;
+  if(!power)
+  {
+     //no power, long beep warning
+     longBeep();
+  }
+}
+
+
+void displayOnRestState(bool isonrest)
+{
+  
+  /*if (isonrest == oldIsOnRest)
+  {
+    //same value, no need to display again.
+    Serial.println("same onrest quit");
+
+    return;
+  }*/
+
+  
+  //oldIsOnRest = isonrest;
+  if (!isonrest)
+  {
+    Serial.println(" !isonrest");
+    return;
+  }
+
+  // if it's onRest, don't print the LCD again too soon.
+  static bool oldIsOnRest = false;
+
+  /*
+   * static unsigned long lastTs = 0;
+  unsigned long nowTs = millis();
+  const unsigned long Delta = 1000;
+  if (( lastTs + Delta )> nowTs )
+  {
+     return;  
+  }
+  
+  lastTs = nowTs;
+  */
+  String text1 = "  on ";   
+  String text2 = " rest";
+  const int x1 = 0, x2=8;
+  lcd.setCursor(x1, 1);
+  lcd.print(text1);
+  lcd.setCursor(x2, 1);
+  lcd.print(text2);
+  Serial.println("display on rest !!");
+ 
+}
+
+// pwm value from 0 to 255
+String getPwmBarFromPwmValue(byte pwm)
+{
+  // limit by the lcd screen space
+  const int MaxBarTypeCount = 5, MaxPWM=255; 
+  const String emptyBar = "     ";
+  //const int NoShow=5;
+  if(pwm == 0)
+  {
+    return emptyBar;
+  }
+  
+  //int index = round((1.0*pwm)/MaxPWM * (MaxBarTypeCount-1));
+  int index= map(pwm, 0, MaxPWM, 0, MaxBarTypeCount-1); 
+  // should not use empty bar as first item, because no matter how small the pwm is, it need to be shown
+  static const String BarString[]={">    ",">>   ",">>>  ",">>>> ",">>>>>"};
+  return BarString[index];
+  
+}
+void showHeaterPwmBar()
+{
+  static unsigned long lastTs = 0;
+  unsigned long nowTs = millis();
+  const unsigned long Delta = 500;
+  if(( nowTs -lastTs ) <Delta )
+  {
+    return;
+  }
+  Serial.println("showHeaterPwmBar");
+
+  lastTs = nowTs;
+  const int xpwmbrown=0, xpwmred=8;
+  lcd.setCursor(xpwmbrown, 1);
+  String bar;
+  bar = getPwmBarFromPwmValue(station.myWellerState.brownPwm);
+  // for  test bar = getPwmBarFromPwmValue(255);
+  lcd.print(bar);  
+  Serial.println("pwm:"+ bar + String(station.myWellerState.brownPwm));
+  
+  bar = getPwmBarFromPwmValue(station.myWellerState.redPwm);
+  // for test  bar = getPwmBarFromPwmValue(255);
+  lcd.setCursor(xpwmred, 1);
+  lcd.print(bar);
+  Serial.println("pwm:"+ bar+ String(station.myWellerState.redPwm));
+  
+}
+
+
+// show tweezer temp and pwm ?
+void showTweezerTemp()
+{
+  //only update temp display once every 800ms, no need to refresh like crazy.
+  const unsigned Delta = 800;
+  static unsigned long last_ts = 0;
+  unsigned long nowts = millis();
+  if ( (nowts - last_ts) < Delta)
+  {
+    return;
+  }
+
+  Serial.println( "showing tweezer temp");
+  last_ts = nowts;
+  String tweezerTemps = "";
+  String red = String(station.myWellerState.redTemp);
+  
+  String brown = String(station.myWellerState.brownTemp);
+  if (station.myWellerState.brownTemp < 100)
+  {
+    brown = " "+brown;
+  }
+
+  if (station.myWellerState.redTemp < 100)
+  {
+    red = " "+red;
+  }
+  
+  //tweezerTemps = String(station.myWellerState.brownPwm)+"  "+brown +  "   " + String(station.myWellerState.redPwm)+ "  "+red;
+  const int xbrown=5, xred=13;
+  lcd.setCursor(xbrown, 1);  
+  lcd.print(brown);
+  lcd.setCursor(xred, 1);
+  lcd.print(red);
+  
+}
+
+
+void measureEnvTemp()
+{
+   //Serial.println( "env temp entering");
+   static unsigned int lastTsSec = 0;
+   unsigned int nowTsSec = millis()/1000;
+   const unsigned int DeltaSec = 30;
+   if (( lastTsSec+DeltaSec ) < nowTsSec )
+   {
+      //Serial.println( "env temp early quiting");
+      return;
+   }
+   //Serial.println( "getting env temp");
+   lastTsSec = nowTsSec;
+   station.disableAllHeaters();
+   delay(2);
+   
+   pinMode(KtyEnablePin, OUTPUT);
+   digitalWrite(KtyEnablePin, HIGH);
+   
+   // when we don't have 12V power , only USB, have to delay longer.
+   delay(2);
+   int ktyInt = station.getAnalogAvgReadingInt(TcInputBlackKty, 3);
+   // int ktyInt= analogRead(TcInputBlackKty);
+   digitalWrite(KtyEnablePin, LOW);
+   // very important, must disable pin high before we leave, or will have wrong adc value for heater temp.
+   pinMode(KtyEnablePin, INPUT);
+   //Serial.println( "kty int " + String(ktyInt));
+
+
+   // float vkty = map(ktyInt, 0, 1023, 0, VCC5 );
+   double vkty = station.adcIntToVoltage(ktyInt);
+   // must not use map, because it returns integer.
+   // vkty = map(ktyInt, 0, 1023, 0, VCC5 );
+   
+   //Serial.println( "vkty  " + String(vkty));
+   double r1k = 991.0; // measure the actual value of the pullup 1k resistor
+   float rkty;
+   
+   // VCC5 /(rkty+r1k ) *rkty = vkty
+   // VCC5 *rkty = vkty *(rkty+r1k)
+   // VCC5*rkty = vkty * rkty + vkty *r1k
+   // VCC5*rkty - vkty*rkty = vkty*r1k
+   // (VCC5-vkty)*rkty = vkty*r1k
+   rkty = vkty*r1k/ ( VCC5-vkty);   
+   //Serial.println("rkty is "+ String(rkty));
+   
+   // https://www.nxp.com/docs/en/data-sheet/KTY82_SER.pdf
+   // Kty82/110,  assuming 80 Ohm per 10C (approximately), so 8 ohm per 1c
+   station.myWellerState.evnTemp = getTempFromKty82_110Resist(rkty);
+   
+}
+
+int getTempFromKty82_110Resist(float rkty)
+{
+  const int Rtemp_sub20=684, Rtemp_sub10=747, Rtemp_0=815, Rtemp_10=886, Rtemp_20= 961, 
+            Rtemp_25=1000, Rtemp_30=1040, Rtemp_40=1122;
+  float temp;
+  // simple solution, <10 and >10 two range
+  if ( rkty< Rtemp_10)
+  {
+    // 5 Ohm per C
+    float deltaOhm = Rtemp_10 - rkty;
+    temp = 10.0- ( deltaOhm/7.1);
+    
+  }else if (rkty<Rtemp_20)
+  { // 10>= C < 20, 
+    float deltaOhm = rkty - Rtemp_10;
+    temp = round(10.0 + deltaOhm/7.5);
+  }else if (rkty<Rtemp_30)
+  {
+     //c>=20
+     float deltaOhm = rkty - Rtemp_20;
+     temp = round(20.0 + deltaOhm/7.9);
+  }else
+  {  // c > 30
+     float deltaOhm = rkty - Rtemp_30;
+     temp = round(30.0 + deltaOhm/8.2);
+  }
+  // "better" solution binary search.
+  return temp;
+}
+
+
+void setup()
+{
+  pinMode(BuzzerEnablePin, OUTPUT);
+  pinMode( TcInputBlackKty , INPUT);
+  
+  beepStart();
+  displayInit();
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Starting");
+  showTitle();
+  beepEnd();
+  //delay(1000);
+  solderStationInit();
+  lcd.clear();
+
+}
+
+bool checkIsHandleOnRest()
+{
+  static unsigned long lastCheckOnRestTs = 0;
+  unsigned long onrest_nowts = millis();
+  const unsigned long delta = 600;
+  // if previous state is working/Not on rest, check on rest immediately
+  bool newOnRest, oldOnRest = station.myWellerState.isOnRest;
+  Serial.println(" checkIsHandleOnRest");
+
+  if ( oldOnRest )
+  { 
+    Serial.println(" if oldOnRest");
+    // if previous state is on rest, delay checking again too soon
+    if (( lastCheckOnRestTs + delta) > onrest_nowts)
+    {
+       // just return the previous value.
+       Serial.println(" quick return");
+       return oldOnRest;
+    }
+  }
+  Serial.println(" finding out magnet");
+
+  newOnRest = station.isPenPutOnRest();
+  lastCheckOnRestTs = onrest_nowts;
+      
+  if ( oldOnRest != newOnRest)
+  {
+    shortBeep();
+  }
+  return newOnRest;
+}
+
+void loop()
+{
+  // if it's ON too long we shut down it.
+  unsigned long startTimeStamp = 0;
+  // store the last start time stamp
+
+  startTimeStamp = millis();
+  measureEnvTemp();
+  bool power12v = checkIs12VPowerConnected();
+  if(!power12v)
+  {
+     //turn off, so maybe we can use a 12V power supply with less wattage output???
+     station.disableAllHeaters();
+  }
+  display12VPower(power12v);
+  
+  if (debug)
+  {
+    if (power12v)
+    {
+      Serial.println("12V is connected");
+    } else
+    {
+      Serial.println("12V disconnected");
+    }
+  }
+  //Serial.println("============ loop starts ===============");
+
+  if (station.checkIsFatalError())
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("Fatal Error");
+    //Serial.println("Fatal Error!");
+    delay(8000);
+    return;
+  }
+
+  if ( rotEncoder.getButtonClicked())
+  {
+    //Serial.println("button clicked");
+    station.myWellerState.workStatus = !station.myWellerState.workStatus;
+    displayOnOffState( );
+    //it's ON but 12V is off??
+    shortBeep();
+    if (station.myWellerState.workStatus == WellerSolderControllerStatus::ON && !power12v)
+    {
+      // power is on by 12V is missing
+      delay(200);
+      shortBeep();
+      delay(200);
+      shortBeep();
+    }
+  }
+
+  // solder station initial state is OFF
+  if ( station.myWellerState.workStatus == WellerSolderControllerStatus::OFF)
+  {
+    turnSolderStationOff(); 
+  } else
+  { // it's ON
+    unsigned int nowTsSec = millis()/1000;
+    if( g_heaterStartTimerSec == 0)
+    {
+      //not started
+      g_heaterStartTimerSec = nowTsSec;
+    }else
+    {
+      //already started, check Max timeout
+      if (  nowTsSec > ( g_heaterStartTimerSec + MaxPowerOnSec ))
+      {
+        Serial.println("time out" + String(g_heaterStartTimerSec) + " " +String(nowTsSec) );
+        turnSolderStationOff(); 
+        longBeep();
+       }
+    }
+    if ( debug )
+    { 
+      Serial.println("Station is ON");
+    }
+
+  }
+  //displayOnOffState( );
+  int targetTemp = getTargetTemperature();
+  displayTargetTemp(targetTemp);
+  //station.processWellerHandleTemp(targetTemp);
+  // have to check temp first to know if it's connected.
+  station.checkHandleTemp();
+
+  if (station.myWellerState.isConnected )
+  {
+    // env temp must be ready before tweezer temp, because later need it.
+    showTweezerTemp();
+
+    //displayHandleState();
+    bool isOnRest = checkIsHandleOnRest();
+    Serial.println("isOnRest " + String( isOnRest));
+    if (isOnRest)
+    {
+      // !!! can be revised later, on rest can be in standby mode, 150C maybe.
+      // turnSolderStationOff(); 
+      // do not set the state off.
+      station.disableAllHeaters();
+      g_heaterStartTimerSec = 0;
+      displayOnRestState(isOnRest);
+      
+
+    } else // not on rest
+    {
+      if( power12v)
+      {
+         // only turn on heat when we have 12V power
+         station.processWellerHandleTemp(targetTemp);
+      }
+      showHeaterPwmBar(); 
+
+    }
+  } else
+  { // handle is NOT connected
+    //disconnect is always off
+    turnSolderStationOff();
+    // myTM1637.display("DSC");
+    // displayStationState( );
+    //displayHandleState();
+    // Serial.println("disconnected");
+
+  } // end of if ( station.myWellerState.isConnected)
+  displayOnOffState( );
+  displayHandleConnectState();
+  
+  /*
+    // update display
+    if (station.myWellerState.workStatus == WellerSolderControllerStatus::OFF )
+    {
+      //myTM1637.display("OFF");
+    } else if (station.myWellerState.workStatus == WellerSolderControllerStatus::ON)
+    {
+      //myTM1637.display(String(newTargetTemp ));
+    }*/
+  //delay();
+  //Serial.println("============ loop end ===============\n");
+
+
+  // calculating sleep time to fit the PID algorithm.
+  unsigned long endTimeStamp = millis();
+  unsigned long timeUsedMs = endTimeStamp - startTimeStamp ;
+  const int ShouldDelayms = 1000 / FreqHz;
+  long int delta = ShouldDelayms  - timeUsedMs;
+
+  if (delta > 0)
+  {
+    Serial.println("timeUsedMs: " + String(timeUsedMs) + " sleep for " + String(delta));
+    delay(delta);
+  }else
+  {
+     Serial.println("timeUsedMs: " + String(timeUsedMs) + " " + String(delta)+ " no sleep");
+  }
+
+}
